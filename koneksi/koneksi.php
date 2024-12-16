d9224adf2c9ff8c4d3b15c350d889e477bf1ac7b
Isal0192 <faisalfajar1305@gmail.com>
2024-12-16 11:27:18 +0700

    first commit

---
+++ koneksi/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "192.0.0.1:3307"
+    $user = "root"
+    $pass = ""
+    $db = "topup"
+
+
+    //koneksi ke databases belum ada (belum ada databases)
+    koneksi = mysqli_connect($host, $user, $pass, $db)
+?>
